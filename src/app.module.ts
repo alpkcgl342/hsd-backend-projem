50901b496a8222be67bf51c2736908d3aaf38d02
alpkcgl342 <alialpkocaoglu70@gmail.com>
2026-07-07 14:17:21 +0300

    feat: add module skeletons for all team members (auth, blog, events, announcements, applications, committees, contact)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BlogModule } from './blog/blog.module';
+import { EventsModule } from './events/events.module';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { ApplicationsModule } from './applications/applications.module';
+import { CommitteesModule } from './committees/committees.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, BlogModule, EventsModule, AnnouncementsModule, ApplicationsModule, CommitteesModule, ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
